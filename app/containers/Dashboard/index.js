da4d85755540e0e411e32d886a3b9e3113866fb2
Đỗ Văn Hoan <vanhoan90it6@gmail.com>
2019-09-08 17:20:27 +0700

    Chart kit intergrated

--- app/containers/Dashboard/index.js
+++ app/containers/Dashboard/index.js
@@ -1,11 +1,52 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Dimensions } from 'react-native';
+
+import {
+  LineChart,
+  BarChart,
+  PieChart,
+  ProgressChart,
+  ContributionGraph,
+  StackedBarChart
+} from 'react-native-chart-kit';
 
 export default class Dashboard extends Component {
   render() {
+    const line = {
+      labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+      datasets: [
+        {
+          data: [20, 45, 28, 80, 99, 43],
+          strokeWidth: 2, // optional
+        },
+      ],
+    };
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Dashboard screen!</Text>
+      <Text>
+      Bezier Line Chart
+    </Text>
+    <LineChart
+      data={line}
+      width={Dimensions.get('window').width} // from react-native
+      height={220}
+      yAxisLabel={'$'}
+      chartConfig={{
+        backgroundColor: '#e26a00',
+        backgroundGradientFrom: '#fb8c00',
+        backgroundGradientTo: '#ffa726',
+        decimalPlaces: 2, // optional, defaults to 2dp
+        color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+        style: {
+          borderRadius: 16
+        }
+      }}
+      bezier
+      style={{
+        marginVertical: 8,
+        borderRadius: 16
+      }}
+    />
       </View>
     );
   }
--- app/containers/Pfm/index.js
+++ app/containers/Pfm/index.js
@@ -5,7 +5,7 @@ import { View , Text} from 'react-native';
 export default class Pfm extends React.Component {
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', marginLeft: 20, marginRight: 20 }}>
         <Text>Personal financial management screen!</Text>
       </View>
     );
